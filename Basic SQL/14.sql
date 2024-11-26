9c77d1b76a1523b8a5cb844efbd03e296c75d588
MCTGiang <143694286+MCTGiang@users.noreply.github.com>
2024-11-26 10:09:05 +0700

    Update 14.sql

--- Basic SQL/14.sql
+++ Basic SQL/14.sql
@@ -28,8 +28,9 @@
 # A:
 SELECT 
     P.product_id,
-    ROUND(SUM(P.price * U.units)/ SUM(U.units), 2) AS average_price
+    ROUND(COALESCE(SUM(P.price * U.units) / SUM(U.units), 0), 2) AS average_price
 FROM Prices P
-JOIN UnitsSold U ON P.product_id = U.product_id
-AND U.purchase_date BETWEEN P.start_date AND P.end_date
+LEFT JOIN UnitsSold U 
+    ON P.product_id = U.product_id
+    AND U.purchase_date BETWEEN P.start_date AND P.end_date
 GROUP BY P.product_id;
